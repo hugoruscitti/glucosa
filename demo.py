fc56929f12a3181dfbde61ea27de3a3d0f21d243
Quique Porta <quiqueporta@gmail.com>
2012-05-21 12:32:17 +0200

    Primera aproximación a la captura de eventos.

--- demo.py
+++ demo.py
@@ -14,8 +14,22 @@ def create_window():
     window = gtk.Window()
     window.connect('destroy', gtk.main_quit)
     canvas = gtk.DrawingArea()
+    
+    # Añadimos lo eventos que queremos capturar.
+    # Esta operación se debe hacer previamente a añadir el DrawingArea a
+    # la ventana.    
+    canvas.set_events(  gtk.gdk.BUTTON_PRESS_MASK
+                      | gtk.gdk.BUTTON_RELEASE_MASK
+                      | gtk.gdk.KEY_RELEASE_MASK
+                      | gtk.gdk.KEY_PRESS_MASK)
+    
+    # Perimitmos que el DrawingArea tenga el foco para pdoer capturar los
+    # eventos del teclado.
+    canvas.set_flags (gtk.CAN_FOCUS)
+    
     window.add(canvas)
     window.show_all()
+    
     return canvas
 
 class MainLoop:
@@ -35,7 +49,20 @@ class MainLoop:
         self.widget = widget
         gobject.timeout_add(1000/self.fps, self._update)
         self.widget.connect("expose-event", self._on_draw)
+        self.widget.connect("button-press-event", self._button_press_event)
+        self.widget.connect("button-release-event", self._button_press_event)
+        self.widget.connect("key-press-event", self._key_press_event)
+        self.widget.connect("key-release-event", self._key_press_event)
+        
+        
+    def _key_press_event(self, widget, event):
+        print event
+        return True
 
+    def _button_press_event(self, widget, event):
+        print event
+        return True
+    
     def _update(self):
         self.controller.on_update()
         gobject.idle_add(self.widget.queue_draw)
@@ -46,7 +73,7 @@ class MainLoop:
         window_size = self.widget.get_window().get_size()
         glucosa.fill(context, (50,50,50), window_size)
         self.controller.on_draw(context)
-
+        
 
 class Image:
     """Una imagen simple, que puede ser dibujada por un sprite."""
@@ -165,17 +192,17 @@ class Game:
     def __init__(self):
         self.window = create_window()
         self.mainloop = MainLoop(self, self.window, fps=60)
-        self.actor = Sprite(Image('../clock-cairo/data/terron.png'), 0, 0)
+        #self.actor = Sprite(Image('../clock-cairo/data/terron.png'), 0, 0)
         self.actor_animado = Sprite(Frame('data/moneda.png', 8), 0, 0)
         self.actor_animado.y = 60
         self.texto = Text("Hola Mundo\nBienvenido a Glucosa!", 5, 150)
 
     def on_update(self):        
-        self.actor.x += 1
+        #self.actor.x += 1
         self.actor_animado.update()
 
     def on_draw(self, context):
-        self.actor.draw(context)
+        #self.actor.draw(context)
         self.actor_animado.draw(context)
         self.texto.draw(context)
 
