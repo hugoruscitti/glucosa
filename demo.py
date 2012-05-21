58148b4151bb00a473cbdde0700263e88abdb4ea
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-05-21 18:20:27 -0300

    Moviendo algunas clases al archivo glucosa.py #6

--- demo.py
+++ demo.py
@@ -14,22 +14,22 @@ def create_window():
     window = gtk.Window()
     window.connect('destroy', gtk.main_quit)
     canvas = gtk.DrawingArea()
-    
+
     # Añadimos lo eventos que queremos capturar.
     # Esta operación se debe hacer previamente a añadir el DrawingArea a
-    # la ventana.    
+    # la ventana.
     canvas.set_events(  gtk.gdk.BUTTON_PRESS_MASK
                       | gtk.gdk.BUTTON_RELEASE_MASK
                       | gtk.gdk.KEY_RELEASE_MASK
                       | gtk.gdk.KEY_PRESS_MASK)
-    
+
     # Perimitmos que el DrawingArea tenga el foco para pdoer capturar los
     # eventos del teclado.
     canvas.set_flags (gtk.CAN_FOCUS)
-    
+
     window.add(canvas)
     window.show_all()
-    
+
     return canvas
 
 class MainLoop:
@@ -53,8 +53,8 @@ class MainLoop:
         self.widget.connect("button-release-event", self._button_press_event)
         self.widget.connect("key-press-event", self._key_press_event)
         self.widget.connect("key-release-event", self._key_press_event)
-        
-        
+
+
     def _key_press_event(self, widget, event):
         print event
         return True
@@ -62,7 +62,7 @@ class MainLoop:
     def _button_press_event(self, widget, event):
         print event
         return True
-    
+
     def _update(self):
         self.controller.on_update()
         gobject.idle_add(self.widget.queue_draw)
@@ -73,117 +73,7 @@ class MainLoop:
         window_size = self.widget.get_window().get_size()
         glucosa.fill(context, (50,50,50), window_size)
         self.controller.on_draw(context)
-        
 
-class Image:
-    """Una imagen simple, que puede ser dibujada por un sprite."""
-
-    def __init__(self, path):
-        self.surface = glucosa.load_surface(path)
-
-    def blit(self, context, x, y):
-        glucosa.blit_surface(context, self.surface, x, y)
-
-
-class Frame(Image):
-    """Representa un cuadro de animación, realizado dividiendo una imagen."""
-
-    def __init__(self, path, cols, rows=1):
-        Image.__init__(self, path)
-        self.cols = cols
-        self.rows = rows
-        self.frame_index = 0
-        self.frame_limit = cols * rows
-        self.frame_width = self.surface.get_width() / self.cols
-        self.frame_height = self.surface.get_height() / self.rows
-        
-        self.frame_coordinates = []
-        self.create_frame_coordinates()
-
-    def set_frame(self, index):
-        self.frame_index = index
-
-    def blit(self, context, x, y):
-        #TODO usar glucosa.blit_surface con parametros para que dibuje
-        # solo una parte del tile
-        glucosa.blit_surface(context, self.surface, x, y, 
-                             self.frame_coordinates[self.frame_index][0], 
-                             self.frame_coordinates[self.frame_index][1],
-                             self.frame_width, self.frame_height)
-
-    def create_frame_coordinates(self):
-        """ Calcula las posiciones del cuadro de animación de la Imagen."""
-        
-        cont = 0
-        while cont <= self.frame_limit:
-            frame_col = cont % self.cols
-            frame_row = cont / self.cols
-            
-            dx = frame_col * self.frame_width
-            dy = frame_row * self.frame_height
-            
-            self.frame_coordinates.append([dx, dy])
-            
-            cont += 1
-        
-    def advance(self):
-        """Avanza un cuadro de animación.
-
-        Este método permite hacer animaciones cíclicas fácilmente. Si
-        al momento de avanzar tiene que reiniciar al cuadro 0 lo hace, y
-        retorna True avisando del reinicio."""
-
-        self.frame_index += 1
-
-        if self.frame_index >= self.frame_limit:
-            self.frame_index = 0
-            return True
-
-        return False
-
-class Sprite:
-    """Representa a un personaje dentro del juego."""
-
-    def __init__(self, image, x, y):
-        self.image = image
-        self.x = x
-        self.y = y
-
-    def draw(self, context):
-        self.image.blit(context, self.x, self.y)
-
-    def update(self):
-        if (self.image.__class__.__name__ == "Frame"):
-            self.image.advance()
-
-class Text:
-    """ Muestra un texto en la pantalla """
-    
-    def __init__(self, text, x, y, size=12, color=(0,0,0), face="Monospace"):
-        self.text = text
-        self.x = x
-        self.y = y
-        self.size = size
-        self.color = color
-        self.face = face
-
-    def draw(self, context):
-        
-        lines = self.text.split('\n')
-
-        dy = 0
-        
-        for line in lines:
-            text_width, text_height = glucosa.render_text(context, 
-                                                         self.x, 
-                                                         self.y + dy, 
-                                                         line, 
-                                                         self.color, 
-                                                         self.size,
-                                                         self.face)
-            dy += text_height
-
-    
 class Game:
     """Es el administrador del juego.
 
@@ -195,13 +85,13 @@ class Game:
         self.window = create_window()
         self.mainloop = MainLoop(self, self.window, fps=60)
         #self.actor = Sprite(Image('../clock-cairo/data/terron.png'), 0, 0)
-        self.actor_animado = Sprite(Frame('data/moneda.png', 8), 0, 0)
+        self.actor_animado = glucosa.Sprite(glucosa.Frame('data/moneda.png', 8), 0, 0)
         self.actor_animado.y = 60
-        self.texto = Text("Hola Mundo\nBienvenido a Glucosa!", 5, 150,
+        self.texto = glucosa.Text("Hola Mundo\nBienvenido a Glucosa!", 5, 150,
                           face="Arial",
                           size=18)
 
-    def on_update(self):        
+    def on_update(self):
         #self.actor.x += 1
         self.actor_animado.update()
 
--- glucosa.py
+++ glucosa.py
@@ -45,3 +45,113 @@ def render_text(context, x, y, text, color, size, face):
     context.show_text(text)
 
     return context.text_extents(text)[2:4]
+
+# Sume object oriented stuff
+
+class Image:
+    """Una imagen simple, que puede ser dibujada por un sprite."""
+
+    def __init__(self, path):
+        self.surface = load_surface(path)
+
+    def blit(self, context, x, y):
+        blit_surface(context, self.surface, x, y)
+
+class Frame(Image):
+    """Representa un cuadro de animación, realizado dividiendo una imagen."""
+
+    def __init__(self, path, cols, rows=1):
+        Image.__init__(self, path)
+        self.cols = cols
+        self.rows = rows
+        self.frame_index = 0
+        self.frame_limit = cols * rows
+        self.frame_width = self.surface.get_width() / self.cols
+        self.frame_height = self.surface.get_height() / self.rows
+
+        self.frame_coordinates = []
+        self.create_frame_coordinates()
+
+    def set_frame(self, index):
+        self.frame_index = index
+
+    def blit(self, context, x, y):
+        #TODO usar glucosa.blit_surface con parametros para que dibuje
+        # solo una parte del tile
+        blit_surface(context, self.surface, x, y,
+                             self.frame_coordinates[self.frame_index][0],
+                             self.frame_coordinates[self.frame_index][1],
+                             self.frame_width, self.frame_height)
+
+    def create_frame_coordinates(self):
+        """ Calcula las posiciones del cuadro de animación de la Imagen."""
+
+        cont = 0
+        while cont <= self.frame_limit:
+            frame_col = cont % self.cols
+            frame_row = cont / self.cols
+
+            dx = frame_col * self.frame_width
+            dy = frame_row * self.frame_height
+
+            self.frame_coordinates.append([dx, dy])
+
+            cont += 1
+
+    def advance(self):
+        """Avanza un cuadro de animación.
+
+        Este método permite hacer animaciones cíclicas fácilmente. Si
+        al momento de avanzar tiene que reiniciar al cuadro 0 lo hace, y
+        retorna True avisando del reinicio."""
+
+        self.frame_index += 1
+
+        if self.frame_index >= self.frame_limit:
+            self.frame_index = 0
+            return True
+
+        return False
+
+
+class Sprite:
+    """Representa a un personaje dentro del juego."""
+
+    def __init__(self, image, x, y):
+        self.image = image
+        self.x = x
+        self.y = y
+
+    def draw(self, context):
+        self.image.blit(context, self.x, self.y)
+
+    def update(self):
+        if (self.image.__class__.__name__ == "Frame"):
+            self.image.advance()
+
+class Text:
+    """ Muestra un texto en la pantalla """
+
+    def __init__(self, text, x, y, size=12, color=(0,0,0), face="Monospace"):
+        self.text = text
+        self.x = x
+        self.y = y
+        self.size = size
+        self.color = color
+        self.face = face
+
+    def draw(self, context):
+
+        lines = self.text.split('\n')
+
+        dy = 0
+
+        for line in lines:
+            text_width, text_height = render_text(context,
+                                                         self.x,
+                                                         self.y + dy,
+                                                         line,
+                                                         self.color,
+                                                         self.size,
+                                                         self.face)
+            dy += text_height
