3dc0a7cbeecd69ccc6e6732336544fe2e8736b0d
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-05-26 17:57:56 -0300

    creando clase sound de prueba

--- glucosa.py
+++ glucosa.py
@@ -173,7 +173,7 @@ class Text:
 
 
 class Singleton(type):
-    """ Clase para garantizar que una clase sólo tenga una instancia y 
+    """ Clase para garantizar que una clase sólo tenga una instancia y
     proporcionar un punto de acceso global a ella.
     """
     def __init__(cls, name, bases, dic):
@@ -187,27 +187,27 @@ class Singleton(type):
 
 
 class _EventsManager:
-    
+
     # Listado de eventos a capturar.
     __events__ = ('on_mouse_move',
                   'on_mouse_button_pressed',
                   'on_mouse_button_released',
                   'on_key_pressed',
                   'on_key_released' )
-    
+
     def __getattr__(self, name):
         if hasattr(self.__class__, '__events__'):
             assert name in self.__class__.__events__, \
             "Event '%s' is not declared" % name
         self.__dict__[name] = ev = _EventSlot(name)
         return ev
-    
+
     def __repr__(self): return 'Events' + str(list(self))
-   
+
     __str__ = __repr__
-   
+
     def __len__(self): return NotImplemented
-   
+
     def __iter__(self):
         def gen(dictitems = self.__dict__.items()):
             for attr, val in dictitems:
@@ -218,62 +218,62 @@ class _EventsManager:
 class _EventSlot:
     """ Evento generico al que se agregan observadores para ser informados
     de cuando se ha producido dicho evento. """
-    
+
     def __init__(self, name):
         self.targets = []
         self.__name__ = name
-        
+
     def __repr__(self):
         return 'event ' + self.__name__
-    
+
     def __call__(self, *a, **kw):
         for f in self.targets: f(*a, **kw)
-    
+
     def __iadd__(self, f):
         self.targets.append(f)
         return self
-    
+
     def __isub__(self, f):
         while f in self.targets: self.targets.remove(f)
         return self
 
 class Events(_EventsManager, object):
-    """ Gestor de los posibles eventos que se producen en glucosa. 
-    
+    """ Gestor de los posibles eventos que se producen en glucosa.
+
     >>> def boton_mouse_presionado(self, evento):
     >>>     print evento
     >>>
-    >>> eventos = glucosa.Events(self.window) 
+    >>> eventos = glucosa.Events(self.window)
     >>> eventos.on_mouse_button_pressed += self.boton_mouse_presionado
-    
+
     """
-    
+
     # Solo puede existir una instancia de este objeto en el programa.
     __metaclass__ = Singleton
-    
+
     def __init__(self, widget):
-        
+
         self._widget = widget
-        
+
         # Conectamos los eventos de GTK.
-        self._widget.connect('motion-notify-event', 
+        self._widget.connect('motion-notify-event',
                              self._mouse_move)
-        self._widget.connect('button-press-event', 
+        self._widget.connect('button-press-event',
                              self._mouse_button_press)
-        self._widget.connect('button-release-event', 
+        self._widget.connect('button-release-event',
                              self._mouse_button_released)
-        self._widget.connect('key-press-event', 
+        self._widget.connect('key-press-event',
                              self._key_pressed)
-        self._widget.connect('key-release-event', 
+        self._widget.connect('key-release-event',
                              self._key_released)
-    
-    
+
+
     def _mouse_move(self, widget, event):
         mouse_event = {'x' : event.x,
                       'y' : event.y}
         self.on_mouse_move(mouse_event)
         return True
-    
+
     def _mouse_button_press(self, widget, event):
         print event
         mouse_event = {'button' : event.button,
@@ -288,14 +288,24 @@ class Events(_EventsManager, object):
                       'y' : event.y}
         self.on_mouse_button_released(mouse_event)
         return True
-    
+
     def _key_pressed(self, widget, event):
         key_event = {'key' : event.keyval }
         self.on_key_pressed(key_event)
         return True
-    
+
     def _key_released(self, widget, event):
         key_event = {'key' : event.keyval }
         self.on_key_released(key_event)
         return True
+
+
+class Sound:
+
+    def __init__(self, path):
+        self.path
+
+    def play(self):
+        pass
+
 
