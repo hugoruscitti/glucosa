c8c774c4b39b3e8a4a35d0a43615eb458c851366
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-06-04 16:30:27 -0300

    Creando mock-object  para que no falle la generación de documentación cuando se usa gdk

--- docs/source/gtk.py
+++ docs/source/gtk.py
@@ -1,1 +1,11 @@
 print "********* VERSION falsa de gtk, para permitir que ande el build de readthedocs."
+
+class FakeGDK(object):
+
+    def __init__(self):
+        pass
+
+    def __getattr__(self, attr):
+        pass
+
+gdk = FakeGDK()
