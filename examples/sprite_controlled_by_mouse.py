0423e1e532deeef2f3b756e2500ae44f624e7dc9
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-06-11 16:53:30 -0300

    moviendo el codigo de MainLoop

--- examples/sprite_controlled_by_mouse.py
+++ examples/sprite_controlled_by_mouse.py
@@ -10,34 +10,6 @@ import gobject
 import glucosa
 
 
-class MainLoop:
-    """Representa el bucle principal de un juego.
-
-    Tiene un metodo especial llamado set_controller, en donde
-    uno tiene que especificar el objeto que quiere colocar cómo
-    administrador del juego.
-    """
-
-    def __init__(self, controller, widget, fps=60):
-        self.fps = fps
-        self._set_controller(controller, widget)
-
-    def _set_controller(self, controller, widget):
-        self.controller = controller
-        self.widget = widget
-        gobject.timeout_add(1000/self.fps, self._update)
-        self.widget.connect("expose-event", self._on_draw)
-
-    def _update(self):
-        self.controller.on_update()
-        gobject.idle_add(self.widget.queue_draw)
-        return True
-
-    def _on_draw(self, event, a):
-        context = self.widget.window.cairo_create()
-        window_size = self.widget.get_window().get_size()
-        glucosa.fill(context, (50,50,50), window_size)
-        self.controller.on_draw(context)
 
 class Game:
     """Es el administrador del juego.
@@ -48,7 +20,7 @@ class Game:
 
     def __init__(self):
         (self.window, self.canvas) = glucosa.create_window()
-        self.mainloop = MainLoop(self, self.canvas, fps=60)
+        self.mainloop = glucosa.MainLoop(self, self.canvas, fps=60)
 
         image = glucosa.Image('../data/aceituna.png')
         self.sprite = glucosa.Sprite(image, 0, 0, 18, 18, scale=2)
